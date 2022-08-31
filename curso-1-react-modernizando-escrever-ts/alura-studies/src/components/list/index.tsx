772dad3038b4bbc531dc7a714ab29c914e769b36
robertalxa <robert.alexandrea7@gmail.com>
2022-08-30 22:41:33 -0300

    Introduzindo states, adicionando nova task ao array a cada click no h2 de estudos do dia e renderizando a lista de tasks a cada adição

--- curso-1-react-modernizando-escrever-ts/alura-studies/src/components/list/index.tsx
+++ curso-1-react-modernizando-escrever-ts/alura-studies/src/components/list/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './item';
 import style from './list.module.scss';
 
 function List() {
-    const tasks = [
-        {
-            name: 'React',
-            time: '02:00:00'
-        },
-        {
-            name: 'JavaScript',
-            time: '01:00:00'
-        },
-    ]
+    const [ tasks, setTasks ] = useState(
+        [
+            {
+                name: 'React',
+                time: '02:00:00'
+            },
+            {
+                name: 'JavaScript',
+                time: '01:00:00'
+            },
+        ]);
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, { name: 'Estudar estado', time: '03:00:00' }]);
+            }}>Estudos do dia</h2>
             <ul>
                 {tasks.map((task, index) => (
                     <Item
