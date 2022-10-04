0b8e97d7101129f9dfecb1f45cee36707bbcb86c
robertalxa <robert.alexandrea7@gmail.com>
2022-10-04 20:34:12 -0300

    Pegando todos os filtros selecionados e exibindo no span

--- curso-2-react-arquivos-estaticos/aluroni/src/pages/Cardapio/Ordenador/index.tsx
+++ curso-2-react-arquivos-estaticos/aluroni/src/pages/Cardapio/Ordenador/index.tsx
@@ -4,22 +4,40 @@ import { useState } from 'react';
 import classNames from 'classnames';
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
 
-export default function Ordenador() {
+interface Props {
+    ordenador: string,
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({
+    ordenador,
+    setOrdenador
+}: Props) {
     const [status, setStatus] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
     return (
     <button 
-        className={styles.ordenador} 
+        className={
+            classNames({
+                [styles.ordenador] : true,
+                [styles['ordenador--ativo']] : ordenador !== '',
+            })
+        } 
         onClick={() => setStatus(!status)}
         onBlur={() => setStatus(false)} // evento de click fora do elemento
     >
-        <span>Ordenar Por</span>
+        <span>{nomeOrdenador || 'Ordenar Por'}</span>
         {status ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
         <div className={classNames({
             [styles.ordenador__options] : true,
             [styles["ordenador__options--ativo"]] : status
         })}>
             {opcoes.map(opcao => (
-                <div className={styles.ordenador__option} key={opcao.value}>
+                <div 
+                    className={styles.ordenador__option} 
+                    key={opcao.value}
+                    onClick={() => setOrdenador(opcao.value)}
+                >
                     {opcao.nome}
                 </div>
             ))}
