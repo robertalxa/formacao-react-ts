e727c13db27c75716e1d87987a30c5275a1396b4
robertalxa <robert.alexandrea7@gmail.com>
2022-10-04 20:23:16 -0300

    Trabalhando com o status interno do ordenador, controlando se está aberto ou fechado, e mostrando icone de acordo com o estado

--- curso-2-react-arquivos-estaticos/aluroni/src/pages/Cardapio/Ordenador/index.tsx
+++ curso-2-react-arquivos-estaticos/aluroni/src/pages/Cardapio/Ordenador/index.tsx
@@ -1,11 +1,23 @@
 import styles from './Ordenador.module.scss';
 import opcoes from './opcoes.json';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
 
 export default function Ordenador() {
+    const [status, setStatus] = useState(false);
     return (
-    <button className={styles.ordenador}>
+    <button 
+        className={styles.ordenador} 
+        onClick={() => setStatus(!status)}
+        onBlur={() => setStatus(false)} // evento de click fora do elemento
+    >
         <span>Ordenar Por</span>
-        <div className={styles.ordenador__options}>
+        {status ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+        <div className={classNames({
+            [styles.ordenador__options] : true,
+            [styles["ordenador__options--ativo"]] : status
+        })}>
             {opcoes.map(opcao => (
                 <div className={styles.ordenador__option} key={opcao.value}>
                     {opcao.nome}
